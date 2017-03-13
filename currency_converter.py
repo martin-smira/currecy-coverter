779640e267b47e0a18ea7802c980439ace00dace
neekro <smira.martin@gmail.com>
2017-03-13 03:02:45 +0100

    add currency converter

---
+++ currency_converter.py
@@ -0,0 +1,49 @@
+
+import sys
+import getopt
+import requests
+import json
+
+
+def symbolSwitch(x):
+    return {
+        "$": "USD",
+        "€": "EUR",
+        "£": "GBP",
+        "¥": "CNY",
+    }.get(x, x)
+
+
+def main(argv):
+
+    opts, args = getopt.getopt(argv, "", ["amount=", "input_currency=", "output_currency="])
+    output_currency_stated = False
+
+    for opt, arg in opts:
+        if opt == "--amount":
+            amount = float(arg)
+        elif opt == "--input_currency":
+            input_currency = symbolSwitch(arg)
+        elif opt == "--output_currency":
+            output_currency = symbolSwitch(arg)
+            output_currency_stated = True
+
+    api_params = {"base": input_currency}
+
+    if output_currency_stated:
+        api_params["symbols"] = output_currency
+
+    data = requests.get("http://api.fixer.io/latest", params=api_params)
+    dataJ = json.loads(data.text)
+
+    output_rates = {k: "%.2f" % round(float(v) * amount, 2) for k, v in dataJ["rates"].items()}
+
+    output_final_json = {"input":
+                            {"amount": "%.2f" % amount,
+                             "currency": input_currency},
+                         "output": output_rates}
+
+    print(json.dumps(output_final_json, indent=4))
+
+
+main(sys.argv[1:])
